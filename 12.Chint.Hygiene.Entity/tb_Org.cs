25a71dcecef1e51daf31c654e5113cc4b7c09e3a
旭阳 何 <522707831@qq.com>
2020-02-16 16:43:38 +0800

    新增Org

---
+++ 12.Chint.Hygiene.Entity/tb_Org.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace _12.Chint.Hygiene.Entity
+{
+    public class tb_Org
+    {
+        public long ID { get; set; }
+        public long DeptID { get; set; }
+        public string DeptName{ get; set; }
+        public string LeaderOpenID { get; set; }
+        public string LeaderName { get; set; }
+        public string ParentID { get; set; }
+    }
+}
